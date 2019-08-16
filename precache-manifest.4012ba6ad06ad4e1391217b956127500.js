ffc4bad9c2a74c9d49fc3b290909497008f5b3cc
Daniels@dual4t.com <daniels@dual4t.com>
2019-08-15 18:06:21 -0700

    Updates

---
+++ precache-manifest.4012ba6ad06ad4e1391217b956127500.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a539c88e57cfcc9b5b814871c3c916ce",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "fb787ee4302d5a752868",
+    "url": "/memory-game/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "fb787ee4302d5a752868",
+    "url": "/memory-game/static/js/2.d375ea33.chunk.js"
+  },
+  {
+    "revision": "91f85d47a7b3f06808d7",
+    "url": "/memory-game/static/js/main.9fae1c1b.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
